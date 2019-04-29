78d854e6cf207866d9323d5e97b704c01716f476
realsung <sungjun0208@naver.com>
2019-04-30 01:26:30 +0900

    asd

--- example.py
+++ example.py
@@ -2,43 +2,43 @@ import random
 
 print("Level 1")
 for i in range(2, 101):
-	a = random.randint(1, 100)
-	b = random.randint(1, 100)
-	c = random.choice(['+', '-', '*', '/'])
-	if(a < b):
-		while(1):
-			a = random.randint(1, 100)
-			b = random.randint(1, 1)
-			if(a > b):
-				break
-			else:
-				continue
-	print(a, c, b)
-	d = input()
-	if(c == '+'):
-		if(a + b == int(d)):
-			print("Level :", i)
-		else:
-			print("Wrong Answer")
-			break
-	elif(c == '*'):
-		if(a * b == int(d)):
-			print("Level :", i)
-		else:
-			print("Wrong Answer")
-			break
-	elif(c == '/'):
-		if(a / b == int(d)):
-			print("Level :", i)
-		else:
-			print("Wrong Answer")
-			break
-	else:
-		if(a - b == int(d)):
-			print("Level :", i)
-		else:
-			print("Wrong Answer")
-			break
+   a = random.randint(1, 100)
+   b = random.randint(1, 100)
+   c = random.choice(['+', '-', '*', '/'])
+   if(a < b):
+      while(1):
+         a = random.randint(1, 100)
+         b = random.randint(1, 100)
+         if(a > b):
+            break
+         else:
+            continue
+   print(a, c, b)
+   d = input()
+   if(c == '+'):
+      if(a + b == int(d)):
+         print("Level :", i)
+      else:
+         print("Wrong Answer")
+         break
+   elif(c == '*'):
+      if(a * b == int(d)):
+         print("Level :", i)
+      else:
+         print("Wrong Answer")
+         break
+   elif(c == '/'):
+      if(a / b == float(d)):
+         print("Level :", i)
+      else:
+         print("Wrong Answer")
+         break
+   else:
+      if(a - b == int(d)):
+         print("Level :", i)
+      else:
+         print("Wrong Answer")
+         break
 
 if(i == 100):
-	print("FLAG{Y0ur_C4n_Us3_Python}")
+   print("FLAG{Y0ur_C4n_Us3_Python}")
